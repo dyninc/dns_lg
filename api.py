2e6165e42939783e274de33c54b37b48118902d9
JY <jyoung@dyn.com>
2013-08-22 11:13:59 -0400

    remove indents so that file will run.

--- api.py
+++ api.py
@@ -26,43 +26,43 @@ def crossdomain(origin=None, methods=None, headers=None,
                 automatic_options=True):
   """This method allows for Cross-origin resource sharing (CORS). It is used as
   a decorator for the route methods below."""
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if headers is not None and not isinstance(headers, basestring):
-        headers = ', '.join(x.upper() for x in headers)
-    if not isinstance(origin, basestring):
-        origin = ', '.join(origin)
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
+  if methods is not None:
+      methods = ', '.join(sorted(x.upper() for x in methods))
+  if headers is not None and not isinstance(headers, basestring):
+      headers = ', '.join(x.upper() for x in headers)
+  if not isinstance(origin, basestring):
+      origin = ', '.join(origin)
+  if isinstance(max_age, timedelta):
+      max_age = max_age.total_seconds()
 
-    def get_methods():
-        if methods is not None:
-            return methods
+  def get_methods():
+      if methods is not None:
+          return methods
 
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
+      options_resp = current_app.make_default_options_response()
+      return options_resp.headers['allow']
 
-    def decorator(f):
-        def wrapped_function(*args, **kwargs):
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
+  def decorator(f):
+      def wrapped_function(*args, **kwargs):
+          if automatic_options and request.method == 'OPTIONS':
+              resp = current_app.make_default_options_response()
+          else:
+              resp = make_response(f(*args, **kwargs))
+          if not attach_to_all and request.method != 'OPTIONS':
+              return resp
 
-            h = resp.headers
+          h = resp.headers
 
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
+          h['Access-Control-Allow-Origin'] = origin
+          h['Access-Control-Allow-Methods'] = get_methods()
+          h['Access-Control-Max-Age'] = str(max_age)
+          if headers is not None:
+              h['Access-Control-Allow-Headers'] = headers
+          return resp
 
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
+      f.provide_automatic_options = False
+      return update_wrapper(wrapped_function, f)
+  return decorator
 
 def rest_args(*args):
   """Parse the REST arguments for allowed arguments."""
